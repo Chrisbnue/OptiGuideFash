b1b1e576b6b1d659975c684a4ec15f9f56dc43af
Chrisbnue <37227091+Chrisbnue@users.noreply.github.com>
2023-11-09 10:19:16 +0100

    Update fashionsc.py

--- benchmark/application/fashionsc.py
+++ benchmark/application/fashionsc.py
@@ -14,11 +14,11 @@ shipping_cost_from_supplier_to_DC = {
     ('supplier3', 'LDC'): 7
 }
 
-roasting_cost_light = {'CDC': 3, 'LDC': 5}
+inventory_cost_ftw = {'CDC': 3, 'LDC': 5}
 
-roasting_cost_dark = {'CDC': 5, 'LDC': 6}
+inventory_cost_apparel = {'CDC': 5, 'LDC': 6}
 
-shipping_cost_from_DC_to_cafe = {
+shipping_cost_from_DC_to_store = {
     ('CDC', 'Berlin-CS'): 5,
     ('CDC', 'Paris-CS'): 3,
     ('CDC', 'LDN-CS'): 6,
@@ -27,11 +27,11 @@ shipping_cost_from_DC_to_cafe = {
     ('LDC', 'LDN-CS'): 2
 }
 
-light_coffee_needed_for_cafe = {'Berlin-CS': 20, 'Paris-CS': 30, 'LDN-CS': 40}
+ftw_needed_for_stores = {'Berlin-CS': 20, 'Paris-CS': 30, 'LDN-CS': 40}
 
-dark_coffee_needed_for_cafe = {'Berlin-CS': 20, 'Paris-CS': 20, 'LDN-CS': 100}
+app_needed_for_stores = {'Berlin-CS': 20, 'Paris-CS': 20, 'LDN-CS': 100}
 
-cafes = list(set(i[1] for i in shipping_cost_from_DC_to_cafe.keys()))
+cafes = list(set(i[1] for i in shipping_cost_from_DC_to_store.keys()))
 roasteries = list(
     set(i[1] for i in shipping_cost_from_supplier_to_DC.keys()))
 suppliers = list(
@@ -46,10 +46,10 @@ model = Model("coffee_distribution")
 x = model.addVars(shipping_cost_from_supplier_to_DC.keys(),
                   vtype=GRB.INTEGER,
                   name="x")
-y_light = model.addVars(shipping_cost_from_DC_to_cafe.keys(),
+y_light = model.addVars(shipping_cost_from_DC_to_store.keys(),
                         vtype=GRB.INTEGER,
                         name="y_light")
-y_dark = model.addVars(shipping_cost_from_DC_to_cafe.keys(),
+y_dark = model.addVars(shipping_cost_from_DC_to_store.keys(),
                        vtype=GRB.INTEGER,
                        name="y_dark")
 
@@ -57,11 +57,11 @@ y_dark = model.addVars(shipping_cost_from_DC_to_cafe.keys(),
 model.setObjective(
     sum(x[i] * shipping_cost_from_supplier_to_DC[i]
         for i in shipping_cost_from_supplier_to_DC.keys()) +
-    sum(roasting_cost_light[r] * y_light[r, c] +
-        roasting_cost_dark[r] * y_dark[r, c]
-        for r, c in shipping_cost_from_DC_to_cafe.keys()) + sum(
-            (y_light[j] + y_dark[j]) * shipping_cost_from_DC_to_cafe[j]
-            for j in shipping_cost_from_DC_to_cafe.keys()), GRB.MINIMIZE)
+    sum(inventory_cost_ftw[r] * y_light[r, c] +
+        inventory_cost_apparel[r] * y_dark[r, c]
+        for r, c in shipping_cost_from_DC_to_store.keys()) + sum(
+            (y_light[j] + y_dark[j]) * shipping_cost_from_DC_to_store[j]
+            for j in shipping_cost_from_DC_to_store.keys()), GRB.MINIMIZE)
 
 # Conservation of flow constraint
 for r in set(i[1] for i in shipping_cost_from_supplier_to_DC.keys()):
@@ -69,7 +69,7 @@ for r in set(i[1] for i in shipping_cost_from_supplier_to_DC.keys()):
         sum(x[i] for i in shipping_cost_from_supplier_to_DC.keys()
             if i[1] == r) == sum(
                 y_light[j] + y_dark[j]
-                for j in shipping_cost_from_DC_to_cafe.keys()
+                for j in shipping_cost_from_DC_to_store.keys()
                 if j[0] == r), f"flow_{r}")
 
 # Add supply constraints
@@ -79,14 +79,14 @@ for s in set(i[0] for i in shipping_cost_from_supplier_to_DC.keys()):
             if i[0] == s) <= capacity_in_supplier[s], f"supply_{s}")
 
 # Add demand constraints
-for c in set(i[1] for i in shipping_cost_from_DC_to_cafe.keys()):
+for c in set(i[1] for i in shipping_cost_from_DC_to_store.keys()):
     model.addConstr(
-        sum(y_light[j] for j in shipping_cost_from_DC_to_cafe.keys()
-            if j[1] == c) >= light_coffee_needed_for_cafe[c],
+        sum(y_light[j] for j in shipping_cost_from_DC_to_store.keys()
+            if j[1] == c) >= ftw_needed_for_stores[c],
         f"light_demand_{c}")
     model.addConstr(
-        sum(y_dark[j] for j in shipping_cost_from_DC_to_cafe.keys()
-            if j[1] == c) >= dark_coffee_needed_for_cafe[c],
+        sum(y_dark[j] for j in shipping_cost_from_DC_to_store.keys()
+            if j[1] == c) >= app_needed_for_stores[c],
         f"dark_demand_{c}")
 
 # Optimize model
